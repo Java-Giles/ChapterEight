3f799fa56e0cac1ef9b12a327726c120f8f1509c
Cole Giles <coleagiles@gmailc.com>
2019-10-02 09:50:14 -0500

    updated chapter 8

---
+++ src/Exercises/TwelveInts.java
@@ -0,0 +1,27 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class TwelveInts {
+
+    public static void main(String[] args){
+
+        Scanner inp = new Scanner(System.in);
+
+        int count;
+
+        int[] myArray = new int[12];
+
+        for(int i = 0; i < myArray.length; ++i){
+            System.out.println("Enter integer number " + (i+1) + ":");
+            count = inp.nextInt();
+            myArray[i] = count;
+        }
+        System.out.println();
+
+        for(int j = 0; j < myArray.length; ++j)
+            System.out.println(myArray[j]);
+        for(int k = 0;)
+
+    }
+}
