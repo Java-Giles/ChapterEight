aa3688a2fdbd67e29b3ae61fcbed3f0257843aa4
Cole Giles <coleagiles@gmailc.com>
2019-10-04 09:49:08 -0500

    updated chapter 8

--- src/Exercises/DistanceFromAverage.java
+++ src/Exercises/DistanceFromAverage.java
@@ -8,20 +8,58 @@ public class DistanceFromAverage {
 
 
         double userEntry = 0.0;
-        int count = 0;
+        int count = 1;
         double total = 0.0;
-        double average;
-        double[] twentyDoubles = new double[count];
+        double average = 0;
+        double high = 0;
+        double highDistance = 0;
+        double low = 999999;
+        double lowDistance = 0;
+        double distanceNum;
+        double[] twentyDoubles = new double[20];
 
         Scanner input = new Scanner(System.in);
 
-        while(userEntry != 99999){
-            System.out.println("Enter double number " + (count + 1) +  ":");
-            userEntry = input.nextDouble();
-            twentyDoubles[count] = userEntry;
-            ++count;
-            total = total + userEntry;
+        while(userEntry != 99999) {
+                System.out.println("Enter double number " + (count) +  ":");
+                userEntry = input.nextDouble();
+
+            if(userEntry != 99999){
+                count++;
+                twentyDoubles[count] = userEntry;
+                total = total + userEntry;
+                if(userEntry > high){
+                    high = userEntry;
+                }
+                else if(userEntry < low){
+                    low = userEntry;
+                }
+            }
+
+            else{
+                System.out.println("Ending code...");
+            }
+            average = total / count;
+            highDistance = high - average;
+            lowDistance = (low - (low * 2) + average);
+
+
         }
+        System.out.println("Your entered values are:");
+        for(int i = 2; i < count + 1; ++i){
+            System.out.println(twentyDoubles[i]);
+        }
+        if(highDistance > lowDistance){
+            distanceNum = high;
+            System.out.println("The average of your numbers is " + average +
+                    ". \nThe number furthest from the average is " + distanceNum + ", with a distance of " + highDistance);
+        }
+        else{
+            distanceNum = low;
+            System.out.println("The average of your numbers is " + average +
+                    ". \nThe number furthest from the average is " + distanceNum + ", with a distance of " + lowDistance);
+        }
+
 
     }
 }
